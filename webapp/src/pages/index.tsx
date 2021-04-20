edcabdab26ab3acea8f468f722e2a7adad543cd1
Lars L Skjaeveland <lls@skalar.no>
2021-04-20 21:32:34 +0200

    feat: folder structure and next setup

---
+++ webapp/src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%'}}>
+        Velkommen til takterrasen
+      </div>
+  )
+}
+
+export default (Welcome)
